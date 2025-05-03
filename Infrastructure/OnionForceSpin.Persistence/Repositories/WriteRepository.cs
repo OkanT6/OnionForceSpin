b7f832e7577a60d350c98ed045d7be2528d2c425
Okan Topdemir <1030510173@erciyes.edu.tr>
2025-05-03 19:05:02 +0300

    WriteRepository yapıldı.

--- Infrastructure/OnionForceSpin.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/OnionForceSpin.Persistence/Repositories/WriteRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using OnionForceSpin.Application.Interfaces.Repositories;
+using OnionForceSpin.Domain.Common;
+using OnionForceSpin.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,42 @@ using System.Threading.Tasks;
 
 namespace OnionForceSpin.Persistence.Repositories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _context;
+        public WriteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private DbSet<T> Table { get => _context.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(ICollection<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(()=>Table.Remove(entity));
+        }
+
+        //public async Task SoftDeleteAsync(T entity)
+        //{
+        //    await Task.Run(() => Table.Update(entity));
+            
+        //}
+
+
     }
 }
